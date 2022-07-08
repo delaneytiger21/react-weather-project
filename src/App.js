b52228073f8255500308d1c0ed675b352e5bd811
delaneytiger21 <89812496+delaneytiger21@users.noreply.github.com>
2022-07-08 10:53:39 -0500

    Add basic styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import Weather from "./Weather.js"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
 
+        <Weather />
+
+        <footer>
+          Built by Delaney Tiger, open-sourced on <a href="https://github.com/delaneytiger21/react-weather-project" target="_blank" rel="noreferrer">GitHub</a>  and hosted on <a href="https://sensational-kheer-3375d2.netlify.app/" target="_blank" rel="noreferrer"> Netlify</a>
+        </footer>
+
+      </div>
     </div>
   );
 }
